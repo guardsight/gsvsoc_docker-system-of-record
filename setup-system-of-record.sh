b6365b83f65c8734901c82f7a5c92bc0f1dc8b56
Nathan Coats <ncoats@guardsight.com>
2020-12-16 16:48:35 -0700

    fixing deployment script

--- setup-system-of-record.sh
+++ setup-system-of-record.sh
@@ -4,12 +4,11 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update 
-sudo apt-get install -y docker-ce 
+sudo apt-get install -y git jq docker-ce 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose 
 sudo mkdir -p /opt/secops 
 sudo mkdir -p /logs/HOSTS 
-sudo apt install -y git jq 
 sudo sysctl -w vm.max_map_count=262144
 sudo groupadd -g 1111 logs 
 sudo useradd -u 1111 -g 1111 logs 
